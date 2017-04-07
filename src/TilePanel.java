16638c69922c413b6fe4a7b8312abeef698bb0b7
Simon Anguish <simon.anguish@gmail.com>
2017-04-06 23:14:47 -0400

    Changed tilePanel to use Graphics instead of hack JPanels

--- src/TilePanel.java
+++ src/TilePanel.java
@@ -1,54 +1,42 @@
 import javax.swing.*;
 import java.awt.*;
 
-class TilePanel extends JPanel {
+class TilePanel extends JComponent {
 	int tileSize = 30;
-	TilePanel(Tile tile) {
-		setLayout(new GridLayout(3,3));
+	Tile tile = new Tile(true, true, true, true);
+	TilePanel(Tile thisTile) {
 		setSize(new Dimension(tileSize, tileSize));
 
-		drawTile(tile);
+		tile = thisTile;
 	}
 
-	TilePanel(Tile tile, int newTileSize) {
+	public void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setColor(Color.BLACK);
+
+		g2.fillRect(20,20,20,20);
+
+		if (tile.north) {
+			g2.fillRect(20,0,20,20);
+		}
+
+		if (tile.east) {
+			g2.fillRect(40,20,20,20);
+		}
+
+		if (tile.south) {
+			g2.fillRect(20,40,20,20);
+		}
+
+		if (tile.west) {
+			g2.fillRect(0,20,20,20);
+		}
+	}
+
+	TilePanel(Tile newTile, int newTileSize) {
 		tileSize = newTileSize;
-		setLayout(new GridLayout(3,3));
 		setSize(new Dimension(tileSize, tileSize));
 
-		drawTile(tile);
-	}
-
-	void drawTile(Tile tile) {
-		add(drawWhite());
-		if (tile.north) add(drawBlack());
-		else add(drawWhite());
-		add(drawWhite());
-
-		if (tile.west) add(drawBlack());
-		else add(drawWhite());
-
-		add(drawBlack());
-
-		if (tile.east) add(drawBlack());
-		else add(drawWhite());
-
-		add(drawWhite());
-		if (tile.south) add(drawBlack());
-		else add(drawWhite());
-		add(drawWhite());
-	}
-
-	JPanel drawWhite() {
-		JPanel panel = new JPanel();
-		panel.setBackground(Color.WHITE);
-		panel.setPreferredSize(new Dimension(tileSize/3,tileSize/3));
-		return panel;
-	}
-
-	JPanel drawBlack() {
-		JPanel panel = new JPanel();
-		panel.setBackground(Color.BLACK);
-		panel.setPreferredSize(new Dimension(tileSize/3,tileSize/3));
-		return panel;
+		tile = newTile;
 	}
 }
